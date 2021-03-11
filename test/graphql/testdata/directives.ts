0725fdffd73833d6b6bfc82e2e0ec03e85bec1d7
bramkaashoek <b.kaashoek@labdigital.nl>
2021-03-11 11:44:46 +0100

    add testcase for multiple directives

--- test/graphql/testdata/directives.ts
+++ test/graphql/testdata/directives.ts
@@ -6,4 +6,9 @@ export interface A {
    * @directive cacheControl(maxAge: 10, scope: PRIVATE)
    */
   b: number
+  /**
+   * @directive deprecated
+   * @directive cacheControl(maxAge: 10, scope: PRIVATE)
+   */
+  c: number
 }
