6315ab603dc4f8a8309838332db907727ea9c158
David Weterings <d.weterings@labdigital.nl>
2021-09-28 13:23:30 +0200

    fix(filterable trait): filter per tag instead of one combined filter

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -39,3 +39,16 @@ export const switchArray = <T>(
   type.symbol?.name === 'Array'
     ? ifArray(checker.getTypeArguments(type as ts.TypeReference)[0])
     : other(type)
+
+export const ifNotEmpty = <T, S>(items: T[], callback: (items: T[]) => S) => items.length ? callback(items) : undefined
+
+export const combinations = (array: string[]): string[][] => {
+	const results = [[] as string[]] as string[][]
+	for (const value of array) {
+		const copy = [...results]
+		for (const prefix of copy) {
+			results.push(prefix.concat(value))
+		}
+	}
+	return results.filter((c) => c.length).sort((a, b) => a.length - b.length)
+}
